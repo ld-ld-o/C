f42cf144614cb7c9fd36b823acb392a0bb739800
BEOMYUN <138593635+ld-ld-o@users.noreply.github.com>
2025-04-10 13:22:26 +0900

    Update parentheses2.c

--- SQ/parentheses2.c
+++ SQ/parentheses2.c
@@ -2,40 +2,45 @@
 
 int main(void){
     while(1){
-        char stack[100] = {0};
+        char stack[1000] = {0};
         char c;
         int top=0;
         int stopSwitch = 0;
-        for(int i = 0 ; i<100;i++){
+        int isValid = 1;
+        for(int i = 0 ; i<1000;i++){
             scanf("%c",&c);
             if(c=='.'){
-                if(i==0)stopSwitch = 1; 
+                if(i==0)
+                    stopSwitch = 1; 
                 break;
             }
             if(c=='(') stack[top++] = c;
             else if(c=='[') stack[top++] = c;
             else if(c==']'){
-                if(stack[--top]=='[')
-                    stack[top] = 0;
+                if(top!=0&&stack[top-1]=='[')
+                    stack[--top] = 0;
                 else{
-                    printf("no\n");
+                    isValid = 0;
+                     
                     break;
                 }
             }
             else if(c==')'){
-                if(stack[--top]=='(')
-                    stack[top = 0];
+                if(top!=0&&stack[top-1]=='(')
+                    stack[--top]=0;
                 else{
-                    printf("no\n");
+                    isValid = 0;
                     break;
                 }
             }
         }
         if(stopSwitch)
             break;
-        if(top==0)
+        if(top==0&&isValid)
             printf("yes\n");
-        else printf("no\n");
+        else
+            printf("no\n");
+        while (c != '\n') scanf("%c", &c);
     }
     return 0;
 }
