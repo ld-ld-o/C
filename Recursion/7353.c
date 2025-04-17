e1736d299703ee80e2ddfb23416857fabb9fabc0
BEOMYUN <04beomyun@naver.com>
2025-04-17 17:32:53 +0900

    7353 패쓰!

--- Recursion/7353.c
+++ Recursion/7353.c
@@ -8,49 +8,68 @@ typedef struct Node{
 }Node;
 typedef struct Stack{
     Node* head;
-    int size;
-}Stack;
+    int size; }Stack;
+int init(Stack** forStack,Stack** backStack);
+int webNavigation(Stack* forStack, Stack* backStack);
+void push(Stack* stack,char* str);
+char* pop(Stack* stack);
+int empty(Stack* stack);
+void clearStack(Stack* stack);
+void clear(Stack** forStack,Stack** backStack);
+char* copyString(const char* src) ;
 
 int main(void){
-
+    Stack* forStack;
+    Stack* backStack;
+    int n;
+    scanf("%d",&n);
+    for(int i = 0; i < n; i++){
+        init(&forStack, &backStack);
+        webNavigation(forStack, backStack);
+        clear(&forStack, &backStack);
+    }
     return 0;
 }
-int webNavigation(){
-    char* str;
-    char* currentURL = "http://www.acm.org/";
-    scanf("%s",str);
-    Stack* forStack = (Stack*)malloc(sizeof(Stack));
-    Stack* backStack = (Stack*)malloc(sizeof(Stack));
-    forStack->size = 0;
-    backStack->size = 0;
-
-    if(strcmp(str,"VISIT")){
-        push(backStack,currntURL);
-        clearStack(forStack);
-        scanf("%s",currentURL);
-    }
-    else if(strcmp(str,"BACK")){
-        strcpy(pop(backStack),currentURL);
-        push(forStack,currentURL);
-
-
-    }
-    else if(strcmp(str,"FORWARD")){
-    }
-    else if(strcmp(str,"QUIT")){
+int init(Stack** forStack,Stack** backStack){
+    *forStack = (Stack*)malloc(sizeof(Stack));
+    *backStack = (Stack*)malloc(sizeof(Stack));
+    (*forStack)->size = 0;
+    (*backStack)->size = 0;
+}
+int webNavigation(Stack* forStack, Stack* backStack){
+    char* currentURL = "http://www.acm.org/\0";
+    char str[10];
+    while(1){
+        scanf("%s",str);
+        if(strcmp(str,"VISIT")==0){
+            push(backStack,currentURL);
+            clearStack(forStack);
+            scanf("%s",currentURL);
+        }
+        else if(strcmp(str,"BACK")==0){
+            push(forStack,currentURL);
+            strcpy(pop(backStack),currentURL);
+        }
+        else if(strcmp(str,"FORWARD")==0){
+            push(backStack,currentURL);
+            strcpy(pop(forStack),currentURL);
+        }
+        else if(strcmp(str,"QUIT")==0){
+            break;
+        }   
     }
 }
 void push(Stack* stack,char* str){
     Node* newNode = (Node*)malloc(sizeof(Node));
     if(empty(stack))
-        stack->head=newNode;
-    strcpy(str,newNode->url);
-    Node* current = stack->head;
-    for(int i = 0; i < stack->size -1; i++){
+        (stack)->head=newNode;
+    newNode->url = copyString(str);
+    Node* current = (stack)->head;
+    for(int i = 0; i < (stack)->size -1; i++){
         current = current->next;
     }
-    current->next = ndeNode;
-    stack->size++;
+    current->next = newNode;
+    (stack)->size++;
     return;
 }
 char* pop(Stack* stack){
@@ -60,14 +79,14 @@ char* pop(Stack* stack){
     Node* current = stack->head;
     char* url;
     if(stack->size == 1){
-        strcpy(url,current->url);
+        url = copyString(current->url);
         stack->head == NULL;
         free(current);
     }
     else{
         for(int i = 0; i<stack->size-2 ; i++)
             current = current->next;
-        strcpy(url,current->next->url);
+        url = copyString(current->next->url);
         free(current->next);
         current->next=NULL;
     }
@@ -76,18 +95,31 @@ char* pop(Stack* stack){
 }
 int empty(Stack* stack){
     if(stack->size==0)
-        reuturn 1;
+        return 1;
     return 0;
 }
-void claerStack(Stack* stack){
+void clearStack(Stack* stack){
     Node* current = stack->head;
+    Node* current2;
     for(int i = 0 ; i<stack->size ; i++){
         if(current->next != NULL)
-            Node* current2=current->next;
+            current2=current->next;
         free(current);
-        corrent = current2;
+        current = current2;
     }
     stack->head = NULL;
     stack->size = 0;
     return;
+}
+void clear(Stack** forStack,Stack** backStack){
+    clearStack(*forStack);
+    clearStack(*backStack);
+    *forStack = NULL;
+    *backStack = NULL;
+    return;
+}
+char* copyString(char* src) {
+    char* dst = (char*)malloc(strlen(src) + 1);
+    strcpy(dst, src);
+    return dst;
 }
