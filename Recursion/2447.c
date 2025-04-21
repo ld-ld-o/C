425814499918f92149f32234f937f08f887f3f3a
BEOMYUN <04beomyun@naver.com>
2025-04-21 14:42:25 +0900

    17478

--- Recursion/2447.c
+++ Recursion/2447.c
@@ -8,20 +8,30 @@ int main(void){
     return 0;
 }
 void recursion(int n, int j ,int k, const int x){
+    for(int i = 1 ; i<4;i++){
+
+        for(int j = 1 ; j < 4; j++){
+            if((j)==2&(k)==2)
+                printf(" ");
+            else 
+                printf("*");
+            if(n!=1)
+                recursion(n/3,i,j,x);
+        }
+    }
+
+
     if(n!=1){
-            for(int i = 0 ; i < n; i++){
-                recursion(n,j+1,1,x);
+        for(int i = 1 ; i<4;i++){
+            recursion(n/3,i,j,x);
+            for(int j = 1 ; j < 4; j++){
+                recursion(n/3,i,j,x);
             }
+            if(n==x)
+                printf("\n");
+        }
     }
     else{
-        if((j%3)==2&(k%3)==2)
-            printf(" ");
-        else 
-            printf("*");
-    if(k==x){
-        printf("\n");
-        
-
     }
 }
 
